8a1916177f86082421a6ec1edff316378fde6b9b
Ivan Siderov <ivan.sideroff@gmail.com>
2018-07-10 12:34:05 +0300

    added: working loading indicator

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { View, ActivityIndicator, StyleSheet } from 'react-native'
 
 export default class Loading extends Component {
   render() {
-    return <Text>Loading...</Text>
+    return (
+      <View style={[styles.container, styles.horizontal]}>
+        <ActivityIndicator size='large' />
+      </View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10
+  }
+})
